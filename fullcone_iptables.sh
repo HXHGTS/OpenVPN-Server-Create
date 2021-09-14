bf928ab0934698929f66b69a2a7f3c5ac6513180
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-09-14 11:15:57 +0800

    Update fullcone_iptables.sh

--- fullcone_iptables.sh
+++ fullcone_iptables.sh
@@ -144,10 +144,6 @@ lsmod | grep xt_FULLCONENAT
 
 echo -----------------------------------------------------
 
-iptables -F
-
-iptables -Z
-
 iptables -t nat -A POSTROUTING -o eth0 -j FULLCONENAT
 
 iptables -t nat -A PREROUTING -i eth0 -j FULLCONENAT
